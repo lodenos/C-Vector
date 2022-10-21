28088f5c3bd4d54bb25353306d10d6bbfe5463d9
lodenos <nikolaiilodenos@gmail.com>
2022-10-21 02:29:32 +0200

    fix bounding_box_inside

--- src/bounding_box_inside.c
+++ src/bounding_box_inside.c
@@ -2,16 +2,16 @@
 
 bool vector2f32_bounding_box_inside(Vector2F32* vector, Vector2F32* start,
     Vector2F32* end) {
-  if (((vector->x >= start->x) && (vector->x <= end->x)) &&
-      ((vector->y >= start->y) && (vector->y <= end->y)))
+  if (((vector->x >= start->x) && (vector->y <= start->y)) &&
+      ((vector->x >= end->x) && (vector->y <= end->y)))
     return true;
   return false;
 }
 
 bool vector2f64_bounding_box_inside(Vector2F64* vector, Vector2F64* start,
     Vector2F64* end) {
-  if (((vector->x >= start->x) && (vector->x <= end->x)) &&
-      ((vector->y >= start->y) && (vector->y <= end->y)))
+  if (((vector->x >= start->x) && (vector->y <= start->y)) &&
+      ((vector->x >= end->x) && (vector->y <= end->y)))
     return true;
   return false;
 }
